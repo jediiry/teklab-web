d90f5e433afc7a96e3384691b16404e8f08a26c2
michaelChidi <cmichael@consumermfb.com.ng>
2021-02-18 16:11:12 +0100

    Initial commit

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,10 +2,10 @@
     <div>
         <navbar/>
         <div>
-            <div class="blog__header">
-                <h1 class="text-center my-10">Welcome to TeklabSpace <span class="text-green-500">Blog!</span></h1>
+            <div class="blog__header px-14">
+                <h1 class="text-center my-12">Welcome to TeklabSpace <span class="text-green-500">Blog!</span></h1>
 
-                <div class="blog__content px-10 mb-5">
+                <div class="blog__content mb-5"> 
                     <div class="blog__grid">
                         <div>
                             <img src="../assets/img/blog_img.png" alt="">
@@ -19,6 +19,93 @@
                             <img src="../assets/img/blog_img.png" alt="">
                         </div>
                     </div>
+                </div>
+
+                <div>
+                    <h1 class="mb-4 text-3xl font-bold">Recent Posts </h1>
+                    <div class="flex justify-between mb-8">
+                        <div class="mr-3">
+                            <div class="relative">
+                                <img src="../assets/img/blog_img.png" alt="">
+                                <img class="avatart_img" src="../assets/img/user.jpg" alt="">
+                            </div>
+                            <div class="mt-3">
+                                <p class="text-sm font-bold mb-0">Joker’s Stash Carding Market to Call it Quits</p>
+                                <span class="text-xs font-italic">Uploaded on July 2020</span>
+                            </div>
+                        </div>
+                        <div class="mr-3">
+                            <div class="relative">
+                                <img src="../assets/img/blog_img.png" alt="">
+                                <img class="avatart_img" src="../assets/img/user.jpg" alt="">
+                            </div>
+                            <div class="mt-3">
+                                <p class="text-sm font-bold mb-0">Joker’s Stash Carding Market to Call it Quits</p>
+                                <span class="text-xs font-italic">Uploaded on July 2020</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="relative">
+                                <img src="../assets/img/blog_img.png" alt="">
+                                <img class="avatart_img" src="../assets/img/user.jpg" alt="">
+                            </div>
+                            <div class="mt-3">
+                                <p class="text-sm font-bold mb-0">Joker’s Stash Carding Market to Call it Quits</p>
+                                <span class="text-xs font-italic">Uploaded on July 2020</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-between">
+                        <div class="mr-3">
+                            <div class="relative">
+                                <img src="../assets/img/blog_img.png" alt="">
+                                <img class="avatart_img" src="../assets/img/user.jpg" alt="">
+                            </div>
+                            <div class="mt-3">
+                                <p class="text-sm font-bold mb-0">Joker’s Stash Carding Market to Call it Quits</p>
+                                <span class="text-xs font-italic">Uploaded on July 2020</span>
+                            </div>
+                        </div>
+                        <div class="mr-3">
+                            <div class="relative">
+                                <img src="../assets/img/blog_img.png" alt="">
+                                <img class="avatart_img" src="../assets/img/user.jpg" alt="">
+                            </div>
+                            <div class="mt-3">
+                                <p class="text-sm font-bold mb-0">Joker’s Stash Carding Market to Call it Quits</p>
+                                <span class="text-xs font-italic">Uploaded on July 2020</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="relative">
+                                <img src="../assets/img/blog_img.png" alt="">
+                                <img class="avatart_img" src="../assets/img/user.jpg" alt="">
+                            </div>
+                            <div class="mt-3">
+                                <p class="text-sm font-bold mb-0">Joker’s Stash Carding Market to Call it Quits</p>
+                                <span class="text-xs font-italic">Uploaded on July 2020</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-center my-24">
+                        <div class="flex">
+                            <div class="bg-gray-300 text-white h-8 w-8 rounded-full flex justify-center items-center">
+                                <i class="fas fa-arrow-left"></i>
+                            </div>
+                            <div class="flex mx-5">
+                                <span class="bg-pink text-white h-8 w-8 rounded-full flex font-bold justify-center items-center mr-3">1</span>
+                                <span class="font-bold h-8 w-8 rounded-full flex font-bold justify-center items-center mr-3">2</span>
+                                <span class="font-bold h-8 w-8 rounded-full flex font-bold justify-center items-center mr-3">3</span>
+                                <span class="font-bold h-8 w-8 rounded-full flex font-bold justify-center items-center">4</span>
+                            </div>
+                            <div class="bg-pink text-white h-8 w-8 rounded-full flex justify-center items-center">
+                                <i class="fas fa-arrow-right"></i>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
@@ -45,11 +132,7 @@ export default {
 
 <style>
 .blog__grid{
-    /* display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 25px; */
     display: flex;
-
 }
 
 .blog__grid > :not(:last-child){
@@ -59,12 +142,17 @@ export default {
 .blog__grid > :nth-child(1),
 .blog__grid > :nth-child(3)
 {
-    /* display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 1fr;
-    gap: 25px; */
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+}
+
+.avatart_img{
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    border-radius: 50%;
+    bottom: 9%;
+    right: 6%;
 }
 </style>
