a22edd1a9212a7cedfb990e3b215efd87f31e4c7
michaelChidi <cmichael@consumermfb.com.ng>
2021-02-19 07:47:28 +0100

    All pages iplemented

---
+++ src/views/Careers.vue
@@ -0,0 +1,173 @@
+<template>
+    <div>
+        <navbar/>
+        <image-banner 
+            title="CAREERS"
+            subtitle="Get to know what inspires us, our core values and our Team"
+            img="banner_img.jpg"
+            date=""
+        ></image-banner>
+        <div class="p-16">
+            <div class="grid grid-cols-3 mt-16 mb-8 gap-16">
+                <div class="col-span-2 flex flex-col justify-center py-4">
+                    <h1 class="mb-3 font-bold">Join Our Amazing Team</h1>
+                    <p class="text-sm">Every organization should welcome the assistance of individuals seeking to point out potential vulnerabilities. A Vulnerability Disclosure Policy (VDP) provides clear guidelines to hackers and researchers for reporting vulnerabilities. It's also a best practice and a regulatory expectation. Teklabspace guides hackers to submit findings through the proper channels, ensures submissions are received quickly and in a consistent format, and integrates incoming reports with your existing security workflows for faster remediation. </p>
+                    
+                </div>
+                <div class="col-span-1">
+                    <img src="../assets/img/service_img.png" alt="">
+                </div>
+            </div>
+
+            <div class="flex items-center justify-center">
+                <span class="short__border"></span>
+            </div>
+            <h1 class="mt-3 text-center mb-10 font-bold">BENEFITS</h1>
+            <div>
+                <div class="benefits">
+                    <div class="benefit border border-gray-900 rounded-md p-3">
+                        <span class="mb-4 inline-block shadow-sm rounded-full w-14 h-14 flex items-center justify-center">
+                            <i class="fas fa-check-circle color-pink text-2xl"></i>
+                        </span>
+                        <h3 class="font-bold text-xl mb-3">Work with the best</h3>
+                        <p class="text-xs" style="line-height: 1.6">
+                            You will have the opportunity to help shape the future of the platform, bringing your own flare and skill set to our world-class team.
+                        </p>
+                    </div>
+                    <div class="benefit border border-gray-900 rounded-md p-3">
+                        <span class="mb-4 inline-block shadow-sm rounded-full w-14 h-14 flex items-center justify-center">
+                            <i class="fas fa-check-circle color-pink text-2xl"></i>
+                        </span>
+                        <h3 class="font-bold text-xl mb-3">Excellent benefits</h3>
+                        <p class="text-xs" style="line-height: 1.6">
+                            You will have the opportunity to help shape the future of the platform, bringing your own flare and skill set to our world-class team.
+                        </p>
+                    </div>
+                    <div class="benefit border border-gray-900 rounded-md p-3">
+                        <span class="mb-4 inline-block shadow-sm rounded-full w-14 h-14 flex items-center justify-center">
+                            <i class="fas fa-check-circle color-pink text-2xl"></i>
+                        </span>
+                        <h3 class="font-bold text-xl mb-3">Experience over education</h3>
+                        <p class="text-xs" style="line-height: 1.6">
+                            You will have the opportunity to help shape the future of the platform, bringing your own flare and skill set to our world-class team.
+                        </p>
+                    </div>
+                    <div class="benefit border border-gray-900 rounded-md p-3">
+                        <span class="mb-4 inline-block shadow-sm rounded-full w-14 h-14 flex items-center justify-center">
+                            <i class="fas fa-check-circle color-pink text-2xl"></i>
+                        </span>
+                        <h3 class="font-bold text-xl mb-3">Latest technology</h3>
+                        <p class="text-xs" style="line-height: 1.6">
+                            You will have the opportunity to help shape the future of the platform, bringing your own flare and skill set to our world-class team.
+                        </p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="flex items-center justify-center mt-14">
+                <span class="short__border"></span>
+            </div>
+            <h1 class="mt-3 text-center mb-14 font-bold">CURRENT OPENINGS</h1>
+
+            <div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">Content Engineer</p>
+                        <span class="text-sm">LOCATION: West Africa, East Africa, North Africa</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">Sales Executive Manager</p>
+                        <span class="text-sm">LOCATION: West Africa, East Africa, North Africa, South Africa</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">Frontend Developer</p>
+                        <span class="text-sm">LOCATION: Remote</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">PHP, C# Developer</p>
+                        <span class="text-sm">LOCATION: Remote</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">Product Manager</p>
+                        <span class="text-sm">LOCATION: West Africa, East Africa, North Africa</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">Graphics Designer</p>
+                        <span class="text-sm">LOCATION: Remote</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+                <div class="bg-gray-300 mb-3 rounded p-3 flex justify-between items-center">
+                    <div>
+                        <p class="font-bold text-lg mb-0">Training Content Developer</p>
+                        <span class="text-sm">LOCATION: West Africa, East Africa, North Africa</span>
+                    </div>
+                    <span>
+                        <i class="fas fa-plus cursor-pointer"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <main-footer></main-footer>
+    </div>
+</template>
+
+<script>
+import navbar from "@/components/extra/navbar";
+import ImageBanner from "@/components/extra/ImageBanner";
+import MainFooter from "@/components/extra/MainFooter";
+
+export default {
+    name: 'Careers',
+    components:{
+        navbar,
+        ImageBanner,
+        MainFooter
+    },
+}
+</script>
+
+<style>
+.short__border{
+    content: '';
+    position: relative;
+    border: 3px solid #DA04F2;
+    width: 5%;
+}
+
+.benefits {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    gap: 36px;
+    width: 80%;
+    margin: 0 auto;
+}
+</style>
